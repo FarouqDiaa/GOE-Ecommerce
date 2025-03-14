9a29c855c14a5a8f9ab08cfcc0ae769518611119
farouqdiaa <farouqdiaaeldin@gmail.com>
2025-03-14 23:38:29 +0200

    ✨ Add ViewModels

---
+++ src/Presentation/ViewModels/User/SignUpViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.ViewModels.User
+{
+    public class SignUpViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name should contain only letters.")]
+        public required string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one letter and one number.")]
+        public required string Password { get; set; }
+    }
+}
