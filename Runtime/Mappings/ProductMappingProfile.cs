6f8ae9d4bcf9d12c3ac7433863acfa7f637738c7
farouqdiaa <farouqdiaaeldin@gmail.com>
2025-03-15 05:43:58 +0200

    🎨 Added mappings

---
+++ Runtime/Mappings/ProductMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Presentation.ViewModels.ProductViewModels;
+using Application.DTOs.ProductDTOs;
+using Domain.Entities;
+
+namespace Runtime.Mappings
+{
+    class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<ProductViewModel, NewProductDTO>().ReverseMap();
+            CreateMap<NewProductDTO, Product>().ReverseMap();
+        }
+    }
+}
