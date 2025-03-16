e57dadb9002e4701fe45af92328bfebc091d8b11
farouqdiaa <farouqdiaaeldin@gmail.com>
2025-03-16 19:46:05 +0200

    🚀 Added responses

---
+++ src/Application/Responses/BaseResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Responses
+{
+    public abstract class BaseResponse
+    {
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
