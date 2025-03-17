a705b05d1eb01c70d9ba6eb2ad1c5ca67abce921
farouqdiaa <farouqdiaaeldin@gmail.com>
2025-03-17 17:20:33 +0200

    🎨 Modified view models

---
+++ src/Presentation/ViewModels/UserViewModels/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Presentation.ViewModels.UserViewModels
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
